a080a029af725e02d5eaf1ea4fc55e4e2ef3b540
RICCARDO.DEVESCOVI <RICCARDO.DEVESCOVI@AULAS51-PC15.itib.local>
2022-11-19 08:57:49 +0100

    Classe Foglio finita

---
+++ src/foglio/FoglioProva.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package foglio;
+
+public class FoglioProva {
+    
+    public static void main(String[] args) {
+        Foglio f = new Foglio(5, 2, "rosso");
+        
+        f.calcArea();
+    }
+    
+}
